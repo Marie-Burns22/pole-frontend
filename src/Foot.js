37405a2e3c97c6ca321d937772082965c16ff6fc
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-07 15:35:57 -0700

    Remove extra info from Footer. Add logos to image file.

--- src/Foot.js
+++ src/Foot.js
@@ -10,35 +10,15 @@ class Foot extends Component {
                     <div className="container">
                         <div className="row">
                             <div className="col-8 col-12-medium">
-                                <section>
-                                    <header>
-                                        <h2>Footer</h2>
-                                    </header>
-                                    <ul className="dates">
-                                        <li>
-                                            <span className="date">Jan Foot <strong>27</strong></span>
-                                            <h3><a href="#">Lorem dolor sit amet veroeros</a></h3>
-                                            <p>These can be turned into a article link Component.</p>
-                                        </li>
-                                        <li>
-                                            <span className="date">Jan <strong>23</strong></span>
-                                            <h3><a href="#">Ipsum sed blandit nisl consequat</a></h3>
-                                            <p>Blandit phasellus lorem ipsum dolor tempor sapien tortor hendrerit adipiscing feugiat lorem.</p>
-                                        </li>
-                                    </ul>
-                                </section>
+
                             </div>
                             <div className="col-4 col-12-medium">
                                 <section>
                                     <header>
-                                        <h2>What's this all about? Footer</h2>
+                                        <h2>Vegas in Miami</h2>
                                     </header>
                                     <a href="#" className="image featured"><img src="images/pic10.jpg" alt="" /></a>
-                                    <p>
-                                        This is <strong>Dopetrope</strong> a free, fully responsive HTML5 site template by
-										<a href="http://twitter.com/ajlkn">AJ</a> for <a href="http://html5up.net/">HTML5 UP</a> It's released for free under
-										the <a href="http://html5up.net/license/">Creative Commons Attribution</a> license so feel free to use it for any personal or commercial project &ndash; just don't forget to credit us!
-									</p>
+                                    
                                     <footer>
                                         <ul className="actions">
                                             <li><a href="#" className="button">Find out more</a></li>
@@ -77,7 +57,6 @@ class Foot extends Component {
                                     <ul className="social">
                                         <li><a className="icon brands fa-facebook-f" href="#"><span className="label">Facebook</span></a></li>
                                         <li><a className="icon brands fa-twitter" href="#"><span className="label">Twitter</span></a></li>
-                                        <li><a className="icon brands fa-dribbble" href="#"><span className="label">Dribbble</span></a></li>
                                         <li><a className="icon brands fa-tumblr" href="#"><span className="label">Tumblr</span></a></li>
                                         <li><a className="icon brands fa-linkedin-in" href="#"><span className="label">LinkedIn</span></a></li>
                                     </ul>
