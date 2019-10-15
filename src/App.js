2237b2638e79e123f7cab1726c5a17162041676f
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-14 17:52:29 -0700

    Add navigation sidebar and update css to work with react.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import {Route, Switch } from 'react-router-dom'
+import React, {Component} from 'react';
+
+import {Route, Switch, Link } from 'react-router-dom'
 import './assets/css/main.css'
 
 import Navbar from './PageLayout/Navbar';
@@ -9,21 +10,74 @@ import ServiceContainer from './Services/ServiceContainer';
 import ArticleContainer from './News/ArticleContainer';
 import ClassBookingForm from './bookingRequests/ClassBookingForm';
 
-function App() {
-  return (
-    <div>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={AboutContainer} />
-        <Route exact path="/about" component={AboutContainer} />
-        <Route exact path="/services" component={ServiceContainer} />
-        <Route exact path="/news" component={ArticleContainer} />
-        <Route exact path="/booking" component={ClassBookingForm} />
-      </Switch>
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidebarVisible: false,
+    };
+  }
 
-      <Foot />
-    </div>
-  );
+  toggleSidebarVisibility = () => this.setState({ sidebarVisible: !this.state.sidebarVisible });
+
+    render() {
+      return (
+        <div id="body" className={this.state.sidebarVisible ? 'homepage navPanel-visible' : ''}>
+
+          <div id="titleBar">
+            <a href="#navPanel" onClick={this.toggleSidebarVisibility} className="toggle"></a>
+          </div>
+
+          <div id="navPanel">
+            <nav>
+              <Link to="/" className="link depth-0"
+                style={{ WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)' }}>
+                <span className="indent-0"></span>Home
+						  </Link>
+
+              <Link to="about" className="link depth-1"
+                style={{ WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)' }}>
+                <span className="indent-1"></span>About
+						  </Link>
+
+              <Link to="services" className="link depth-1"
+                style={{ WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)' }}>
+                <span className="indent-1"></span>Services
+						  </Link>
+
+              <Link to="services" className="link depth-2"
+                style={{ WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)' }}>
+                <span className="indent-2"></span>Service Descriptions
+						  </Link>
+
+              <Link to="bookings" className="link depth-2"
+                style={{ WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)' }}>
+                <span className="indent-2"></span>Book Private Sessions
+						  </Link>
+
+              <Link to="news" className="link depth-1"
+                style={{ WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)' }}>
+                <span className="indent-1"></span>News
+						  </Link>
+
+
+            </nav>
+          </div>
+          <div id="page-wrapper">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={AboutContainer} />
+            <Route exact path="/about" component={AboutContainer} />
+            <Route exact path="/services" component={ServiceContainer} />
+            <Route exact path="/news" component={ArticleContainer} />
+            <Route exact path="/booking" component={ClassBookingForm} />
+          </Switch>
+          <Foot />
+        </div>
+        </div>
+      );
+    }
 }
 
 export default App;
--- src/PageLayout/Navbar.js
+++ src/PageLayout/Navbar.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react'
 import {NavLink} from "react-router-dom"
 
 class Navbar extends Component {
+
     render() {
         return (
             <section id="header">
                 <h1><img className="full-logo" src="./images/Logo-Black-NoBackground.png" alt="" /></h1>
                 <nav id="nav">
                     <ul>
-                        <li className="current"><NavLink to="/about" >Home</NavLink></li>
+                        <li className="current"><NavLink to="/" >Home</NavLink></li>
                         <li><NavLink to="/">About</NavLink></li>
                         <li><NavLink to="/services">Services</NavLink></li>
                         <li><NavLink to="/news">News</NavLink></li>
