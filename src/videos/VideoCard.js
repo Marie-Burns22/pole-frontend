d2d1a4590f66bb26e3c6d941ff03b1b9b0ee6880
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-28 14:51:35 -0700

    Add api call to videoContainer and props to video card. Now renders videos from API.

--- src/videos/VideoCard.js
+++ src/videos/VideoCard.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export default function VideoCard() {
+export default function VideoCard(props) {
     return (
         <div>
             <div className="col-4 col-6-medium col-12-small">
                 <section className="box">
                     <div className="embed-responsive embed-responsive-16by9">
-                        <iframe title="Embeds Page" width="560" height="315" src="https://www.youtube.com/embed/L_XuPWTVAyw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe>
+                        <iframe title="Embeds Page" width="560" height="315" src={props.video.vid} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe>
                     </div>
                     <header>
-                        <h3>Video Title</h3>
+                        <h3>{props.video.title}</h3>
                     </header>
-                    <p>Video description</p>
+                    <p>{props.video.description}</p>
                 </section>
             </div>
         </div>
--- src/videos/VideoContainer.js
+++ src/videos/VideoContainer.js
@@ -2,15 +2,66 @@ import React, { Component } from 'react'
 import VideoCard from './VideoCard'
 
 class VideoContainer extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            videos: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://vmpole.herokuapp.com/api/v1/videos', {
+            method: 'GET',
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        videos: result.data
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+
     render() {
-        return (
-            <div>
+        const { error, isLoaded, videos } = this.state;
+        const videoCards = videos.map(v => (
+            <VideoCard key={v.id} video={v.attributes} id={v.id} />
+        ))
+        if (error) {
+            return (
                 <header>
-                    <h2>Performance Videos</h2>
+                    <h2>Error: {error.message}</h2>
                 </header>
-                <VideoCard />
-            </div>
-        )
+            )
+        } else if (!isLoaded) {
+            return <h2>Loading....</h2>
+        } else if (!videos || videos ===  undefined || videos.length === 0) {
+            return <h2> Find Ms. Vegas on Social Media! Links below. </h2>
+        } else {
+            return (
+                <div>
+                    <header>
+                        <h2>Performance Videos</h2>
+                    </header>
+                    {videoCards}
+                </div>
+            )
+        }
     }
 }
 
